b08a86d7c819bf18ee73efd95852844ee1b7608f
Stevefrdnt <cursedsteven@gmail.com>
2022-11-29 19:16:37 +0700

    adding empty script tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,8 @@ export default function Home() {
       <LayoutContainer>
         <RichEditor />
       </LayoutContainer>
+      {/* to tackle antd.css module not found issue */}
+      <script></script>;
     </div>
   );
 }
